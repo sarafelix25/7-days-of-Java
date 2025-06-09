7d8571c05a1e6a8fbcda63b46d226497e286a453
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2025-06-09 03:26:13 +0000

    day 6

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       Project4Challenge.HelloWorldWithSpace();
+       Project6 car = new Project6("Toyota", "CorollaCross", 2025, 130000);
+
+       System.out.println(car.getMarca());
+       System.out.println(car.getModelo());
+       System.out.println(car.getAno());
+       System.out.println("Valor de venda: " + car.valorVenda());
     }
+
 }
