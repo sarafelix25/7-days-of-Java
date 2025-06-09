d7f353f700514e4137f0a57f51e6ce9355b56cdc
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2025-06-09 03:28:44 +0000

    excluding unnecessary code

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       Project6 car = new Project6("Toyota", "CorollaCross", 2025, 130000);
-
-       System.out.println(car.getMarca());
-       System.out.println(car.getModelo());
-       System.out.println(car.getAno());
-       System.out.println("Valor de venda: " + car.valorVenda());
     }
 
 }
